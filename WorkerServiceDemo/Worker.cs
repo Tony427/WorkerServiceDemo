45e8c8e052e44693025ea7c6fd3091c3ba97f888
TonyTsai <tony@unifiedpractice.com>
2020-02-11 22:52:19 +0800

    every 5 second monitor github personal account is alive

--- WorkerServiceDemo/Worker.cs
+++ WorkerServiceDemo/Worker.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
@@ -11,18 +12,39 @@ namespace WorkerServiceDemo
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private HttpClient _httpClient;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
         }
 
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _httpClient = new HttpClient();
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _httpClient.Dispose();
+            return base.StopAsync(cancellationToken);
+        }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                var result = await _httpClient.GetAsync("https://github.com/Tony427");
+                if (result.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("Success {time}", DateTimeOffset.Now);
+                }
+                else
+                {
+                    _logger.LogInformation("Fail {time}", DateTimeOffset.Now);
+                }
+                await Task.Delay(5 * 1000, stoppingToken);
             }
         }
     }
