ba65e4ce56cbb77e115eecdb3b18d1c6eef37e8c
TonyTsai <tony@unifiedpractice.com>
2020-02-11 23:12:34 +0800

    wrap the log to record service start or shut down.

--- WorkerServiceDemo/Program.cs
+++ WorkerServiceDemo/Program.cs
@@ -1,9 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
 
 namespace WorkerServiceDemo
 {
@@ -11,7 +10,31 @@ namespace WorkerServiceDemo
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // once it become a background service. It won't have the debug window. So we have to log it in txt file.
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.File(@"C:\Data\Code\VS2019\WorkerServiceDemo\LogFile.txt")
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up the service");
+                CreateHostBuilder(args).Build().Run();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "There was a problem starting the service");
+                return;
+            }
+            finally
+            {
+                // ensure any message in the buffer will write into log. not disappear.
+                Log.CloseAndFlush();
+            }
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -19,6 +42,7 @@ namespace WorkerServiceDemo
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
-                });
+                })
+            .UseSerilog();
     }
 }
